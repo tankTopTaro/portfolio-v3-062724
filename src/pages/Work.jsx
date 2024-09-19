05e6427e8ccac1094f6fa0d9aff2669f35a37457
tankTopTaro <balmoresk3v1n@gmail.com>
2024-09-19 17:10:00 +0800

    add some stuffs

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, ImageList, ImageListItem, Typography } from "@mui/material"
+import { Box, Container, ImageList, ImageListItem, Link, Typography } from "@mui/material"
 import Showcase from "../components/Showcase"
 import pokedex from "../assets/pokedex.png"
 import sunnyside from "../assets/sunnyside.png"
@@ -17,6 +17,9 @@ function Work() {
   return (
     <Container maxWidth={false}>
         <Showcase workList={workList} />
+        <Typography variant="h4" color='textPrimary' sx={{ marginTop: '4rem', textAlign: 'center'}}>
+          More Projects to come. For now visit my <Link href='https://github.com/tankTopTaro?tab=repositories' underline='none' color='#FCE00C' target='_blank'>Github Page</Link> to see what other things i created.
+        </Typography>
     </Container>
   )
 }
